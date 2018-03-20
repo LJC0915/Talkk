2fa2dff49135eb9846c4c2deb2e9fc6033d3ed75
jiacong <jiacong.lin@shinsoft.com.tw>
2018-03-20 18:21:07 +0800

    add new ui and demo on it

--- index.js
+++ index.js
@@ -11,9 +11,13 @@ app.get('/', (req, res) => {
 app.get('/temp', (req, res) => {
     res.sendFile( __dirname + '/views/template.html');
 });
+app.get('/ct', (req, res) => {
+    res.sendFile( __dirname + '/views/chatandtemp.html');
+});
 
 // 加入線上人數計數
 let onlineCount = 0;
+
 
 // 修改 connection 事件
 io.on('connection', (socket) => {
@@ -21,6 +25,11 @@ io.on('connection', (socket) => {
     onlineCount++;
     // 發送人數給網頁
     io.emit("online", onlineCount);
+    // socket.on("changeroom", (room) => {
+    //     socket.join(room);
+    //     socket.room  = room;
+    //     console.log('join room :' , room);
+    // })
 
     socket.on("greet", () => {
         socket.emit("greet", onlineCount);
@@ -33,6 +42,11 @@ io.on('connection', (socket) => {
     });
 
     socket.on("send", (msg) => {
+
+        console.log(msg);
+        // socket.broadcast.to(channel).emit(msg);
+
+        // io.in(socket.room ).emit("msg", msg);
         io.emit("msg", msg);
     });
 });
